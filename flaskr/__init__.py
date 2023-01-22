6216cc5932c9570deece08ad0e93db6b589200b7
rodrigomora <rmora@dilarce.com>
2023-01-22 10:54:06 -0600

    App running in local

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,6 @@ import os
 
 from flask import Flask
 
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -29,4 +28,14 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
     return app
